56815a7a6d9db92a30877d5b9ee44a2772a95d02
İsmail Hız <147336418+ismailhiz@users.noreply.github.com>
2024-10-20 00:26:05 +0300

    Add files via upload

---
+++ ücüncühafta.php
@@ -0,0 +1,47 @@
+<?php
+
+//isset,unset kullanımı
+/*echo"isset($AdiSoyadi)";
+$AdiSoyadi="ismail hız";
+echo"isset($AdiSoyadi)":
+
+$degisken="Gelişim Üni";
+echo $degisken."<br>";
+unset($degisken);
+echo $degisken;*/
+
+//Dogum yılı hesaplama
+/*$yil1=2024;
+$yil=2005;
+$yas=$yil1-$yil;
+echo "Benim Yaşım".$yas;*/
+
+//Hesap Makinesi
+/*$sayi=35;
+$sayi2=5;
+
+$topla= $sayi + $sayi2;
+$cikar= $sayi - $sayi2;
+$carp= $sayi * $sayi2;
+$bol= $sayi / $sayi2;
+$mod= $sayi % $sayi2;
+$us= $sayi ** $sayi2;
+
+echo    "1.sayının değeri: $sayi"."<br>";
+echo    "2.sayının değeri: $sayi2"."<br>"; 
+echo    "Toplama işleminin sonucu: $topla"."<br>";
+echo    "Çıkarma işleminin sonucu: $cikar"."<br>";
+echo    "Çarpma işleminin sonucu: $carp"."<br>";
+echo    "Bölme işleminin sonucu: $bol"."<br>";
+echo    "Mod alma işleminin sonucu: $mod"."<br>";
+echo    "Us alma işleminin sonucu: $us"."<br>";*/
+
+    //Sayı eksiltme arttırma
+    /*$sayi=15;
+    echo "<u>Yazmış olduğumuz sayının değeri:
+    </u>".$sayi."<br>".
+    "Sayının değerini artırma işlemi: ".++$sayi."
+    <br>".
+    "Sayının değerini azaltma işlemi: ".--$sayi;*/
+
+     ?>
